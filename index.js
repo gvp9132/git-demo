f471725b8886710014462e67f674109b8b1e415b
wjj9132 <974442421@qq.com>
2026-03-18 09:20:00 +0800

    feat:  添加删除任务函数

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function addTask(task) {
   taskItem.textContent = task;
   taskList.appendChild(taskItem);
 }
+
+
+/**
+ * 删除任务函数, 未经过测试
+ * @param {} task 
+ */
+function removeTask(task) {
+  const taskList = document.querySelector('#task-list');
+  const taskItems = taskList.querySelectorAll('li');
+  taskItems.forEach(item => {
+    if (item.textContent === task) {
+      taskList.removeChild(item);
+    }
+  });
+}
